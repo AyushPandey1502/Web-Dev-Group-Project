f56c61469d2af0c09d4e0fd2dfd47602a4ed3c1b
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2024-04-26 18:39:55 +0530

    Worked on Landing pages

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-const list = document.querySelectorAll('.list');
-
-function activeLink() {
-    list.forEach((item) => item.classList.remove('active'));
-    this.classList.add('active');
-}
-
-list.forEach((item) => item.addEventListener('click', activeLink));
-
-
-
-
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
