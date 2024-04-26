814a48aa8c24fc4ed328c7f33c94c4878683e262
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2024-04-26 14:13:45 +0530

    Worked on PopUp Frontend

--- js/script.js
+++ js/script.js
@@ -7,11 +7,15 @@ function activeLink() {
 
 list.forEach((item) => item.addEventListener('click', activeLink));
 
+
+
+
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
+const sectionContainer = document.querySelector('.section-container');
 
 registerLink.addEventListener('click', ()=>{
     wrapper.classList.add('active');
@@ -23,8 +27,14 @@ loginLink.addEventListener('click', ()=>{
 
 btnPopup.addEventListener('click', ()=>{
     wrapper.classList.add('active-popup');
+    toggleBlur();
 });
 
 iconClose.addEventListener('click', ()=>{
     wrapper.classList.remove('active-popup');
+    toggleBlur();
 });
+
+function toggleBlur() {
+    sectionContainer.classList.toggle('blur');
+}
